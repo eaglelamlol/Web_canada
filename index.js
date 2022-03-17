b8b70ba9eb91eb26fb4282869486c9291e8008c2
eaglelamlol <jjacque10@gmail.com>
2022-03-17 19:31:26 +0100

    api nodejs

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.post("/", (req, res )=>{
+    res.send({
+        msg : "hello"
+    })
+
+})
+
+app.listen(port, () => console.log(`App listening on port localhost:${port}/!`))
+
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1998Ocanada1.",
+    database: "test"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM users", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
